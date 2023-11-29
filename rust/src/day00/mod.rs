cbf7f43a8edfe50331d7598b5bc12dfe5a1c7646
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2023-11-28 22:07:22 -0400

    tested aoc day01 with Rust

---
+++ rust/src/day00/mod.rs
@@ -0,0 +1,88 @@
+// https://adventofcode.com/2022/day/1
+use std::fs::File;
+use std::io::{self, BufRead};
+
+struct SourceFile {
+    lines: Vec<String>,
+}
+
+impl SourceFile {
+    fn new(file_path: &str) -> io::Result<Self> {
+        let lines = read_lines(file_path)?;
+        Ok(Self { lines })
+    }
+}
+
+fn read_lines(file_path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(file_path)?;
+    let lines = io::BufReader::new(file).lines().collect::<Result<_, _>>()?;
+    Ok(lines)
+}
+
+type Cals = Vec<i32>;
+
+fn lines_to_cals(lines: Vec<String>) -> Cals {
+    let mut cals: Cals = Vec::new();
+    let mut current_cluster: i32 = 0;
+
+    for line in lines {
+        if line == "" {
+            cals.push(current_cluster);
+            current_cluster = 0;
+        } else {
+            let value: i32 = line.parse().unwrap();
+            current_cluster += value;
+        }
+    }
+
+    // Push the last cluster
+    if current_cluster != 0 {
+        cals.push(current_cluster);
+    }
+
+    cals
+}
+
+fn top_n_cals(mut cals: Cals, n: usize) -> Cals {
+    cals.sort();
+    cals.reverse();
+    let top_n = cals[0..n].to_vec();
+    top_n
+}
+
+fn either_part(file_path: &str, n: usize) -> io::Result<i32> {
+    let source = SourceFile::new(file_path)?;
+    let cals: Vec<i32> = lines_to_cals(source.lines);
+
+    let binding = top_n_cals(cals, n);
+    let max_cals = binding.iter().sum::<i32>();
+
+    println!(
+        "The {n} elf(s) with the highest calorie count is: {}",
+        max_cals
+    );
+    Ok(max_cals)
+}
+
+pub fn part1(file_path: &str) -> io::Result<i32> {
+    either_part(file_path, 1)
+}
+
+pub fn part2(file_path: &str) -> io::Result<i32> {
+    either_part(file_path, 3)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("src/day00/data/test.txt").unwrap(), 24000);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("src/day00/data/test.txt").unwrap(), 45000);
+    }
+}
---
+++ rust/src/main.rs
@@ -0,0 +1,6 @@
+mod day00;
+
+fn main() {
+    day00::part1("src/day00/data/actual.txt").unwrap();
+    day00::part2("src/day00/data/actual.txt").unwrap();
+}
