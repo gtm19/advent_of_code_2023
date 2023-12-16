9c869081695d17ca82de2561676eb08f51fd8a45
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2023-12-16 09:34:41 -0400

    part 1

---
+++ python/tests/test_day_14.py
@@ -0,0 +1,22 @@
+import aoc_2023.day_14 as day_14
+import os
+import pytest
+
+test_input = os.path.join(os.path.dirname(__file__), "data/day_14.txt")
+
+
+@pytest.fixture
+def solution():
+    return day_14.Solution(test_input)
+
+
+def test_part_1(solution):
+    assert solution.part_1() == 136
+
+
+def test_part_2(solution):
+    assert solution.part_2() == -9999  # TODO: add expected result
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
